d9b54707004af50e97b622bdb758f8f0a298c3bb
Ritik220519 <ritik220519@gmail.com>
2025-09-08 13:22:21 +0530

    created server && test Api

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 
 
+app.use("/" , (req , res) =>{
+    res.send("home page")
+})
+
 app.listen(3000 , ()=>{
     console.log("app is listining on port 3000")
 })
